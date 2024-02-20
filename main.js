c72b9efa5576b463523a36a693b863224de2fe71
Admin <reanajb@gmail.com>
2024-02-20 09:30:36 -0500

    updates

--- main.js
+++ main.js
@@ -67,10 +67,12 @@ const colors = ['color-1', 'color-2', 'color-3', 'color-4', 'color-5',
                 'color-6', 'color-7', 'color-8', 'color-9', 'color-10', 'color-11']
 
 
-// function mouseOver(squares){
-//     squares.addEventListener()
-// }
 
+    // function mouseOver(squares){
+    //   squares.addEventListener('mouseover', (event){
+
+    //   })
+    // }
 
 
   for(let square of squares){
